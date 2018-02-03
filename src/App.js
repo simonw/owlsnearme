b16f9ae9c09cbba3c844146589b6ae8f2e92ec5f
Simon Willison <simonw@eventbrite.com>
2018-02-02 18:03:37 -0800

    First version with a working place search form

--- src/App.js
+++ src/App.js
@@ -13,37 +13,56 @@ class App extends Component {
   state = {
     standardPlaces: [],
     communityPlaces: [],
+    placeName: null,
     /* Tokyo:
     lat: 35.66,
     lng: 139.781
     */
-    lng: -122.4494224,
-    lat: 37.8022071
+    lng: null,//-122.4494224,
+    lat: null,//37.8022071
+    nelat: null,
+    nelng: null,
+    swlat: null,
+    swlng: null,
+    q: null
   }
-  componentDidMount() {
+  fetchPlaceData(lat, lng) {
     get(
       'https://api.inaturalist.org/v1/places/nearby', {
         params: {
-          nelat: this.state.lat,
-          nelng: this.state.lng,
-          swlat: this.state.lat,
-          swlng: this.state.lng
+          nelat: lat,
+          nelng: lng,
+          swlat: lat,
+          swlng: lng
         }
       }
     ).then(response => {
       this.setState({
         standardPlaces: cleanPlaces(response.data.results.standard),
         communityPlaces: cleanPlaces(response.data.results.community),
-        // results: response.data.results
       });
     });
+  }
+  fetchSpeciesData() {
+    // Prefers nelat/nelng/swlat/swlng to lat/lng
+    if (!this.state.nelat && !this.state.lat) {
+      return;
+    }
+    const location = this.state.nelat ? {
+      nelat: this.state.nelat,
+      nelng: this.state.nelng,
+      swlat: this.state.swlat,
+      swlng: this.state.swlng
+    } : {
+      lat: this.state.lat,
+      lng: this.state.lng,
+      radius: 50
+    };
     get(
       'https://api.inaturalist.org/v1/observations/species_counts', {
         params: {
           taxon_id: 19350,
-          lat: this.state.lat,
-          lng: this.state.lng,
-          radius: 50
+          ...location
         }
       }
     ).then(response => {
@@ -62,9 +81,7 @@ class App extends Component {
         params: {
           taxon_id: 19350,
           order_by: 'observed_on',
-          lat: this.state.lat,
-          lng: this.state.lng,
-          radius: 50,
+          ...location,
           photos: true
         }
       }
@@ -83,11 +100,47 @@ class App extends Component {
       })});
     });
   }
+  componentDidMount() {
+    this.fetchSpeciesData();
+  }
+  onSubmit(ev) {
+    ev.preventDefault();
+    get(
+      'http://api.inaturalist.org/v1/places/autocomplete', {
+        params: {
+          q: this.state.q
+        }
+      }
+    ).then(response => {
+      const results = response.data.results;
+      if (results.length) {
+        this.setState({
+          lat: parseFloat(results[0].location.split(',')[0]),
+          lng: parseFloat(results[0].location.split(',')[1]),
+          placeName: results[0].display_name
+        });
+        this.fetchSpeciesData();
+      }
+    });
+  }
+  onTextChange(ev) {
+    this.setState({
+      q: ev.target.value
+    });
+  }
   render() {
-    const position = [this.state.lat, this.state.lng];
+    const position = this.state.lat && [this.state.lat, this.state.lng];
     return (
       <div className="App">
-        <Map center={position} zoom={12}>
+        <form action="/" method="GET" onSubmit={this.onSubmit.bind(this)}>
+          <input type="search"
+            onChange={this.onTextChange.bind(this)}
+            placeholder="Search for a place, e.g. San Francisco"
+            value={this.state.q}
+          />
+          <input type="submit" value="Search" />
+        </form>
+        {position && <Map center={position} zoom={12}>
           <TileLayer
             attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -96,7 +149,7 @@ class App extends Component {
             attribution="<a href=&quot;https://www.inaturalist.org/&quot;>iNaturalist</a>"
             url="https://api.inaturalist.org/v1/colored_heatmap/{z}/{x}/{y}.png?taxon_id=19350"
           />
-        </Map>
+        </Map>}
         <pre>
           {JSON.stringify(this.state, null, 2)}
         </pre>
