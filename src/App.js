675ca21474229b9a6fbb2fb82fa100ac697b91f7
Natalie Downe <nat@natbat.net>
2018-02-04 13:04:32 -0800

    observation spotting experiment

--- src/App.js
+++ src/App.js
@@ -546,7 +546,24 @@ class App extends Component {
       {this.state.observations.length !== 0 && <section className="secondary">
         <div className="inner">
 
-          <h2>Recently spotted</h2>
+          <h2>Recently spotted Owls</h2>
+
+          <div className="spotting-list">
+            {this.state.observations.map((o) => (
+            <div className="spotting" key={o.uri}>
+              <div className="img"><a href={o.uri}><img src={o.image_medium} alt="View observation of {o.common_name} on iNaturalist" /></a></div>
+              <h3 className="title"><a href={o.uri}>{o.common_name || o.name}</a> <em className="observation-species">{o.name}</em></h3>
+              <p>Spotted by <a href={`https://www.inaturalist.org/people/${o.user_login}`}>{o.user_name || o.user_login }</a> {o.distance_km && <span>&nbsp;{`${(o.distance_km * (1000/1600)).toFixed(1)} miles away`}&nbsp;</span>} in {o.place_guess} <span title={o.time_observed_at}>{o.time_observed_ago}</span>.</p>
+              <p>
+                {o.is_research ? (
+                  <span className="verify">Verified observation</span>
+                ) : (
+                  <a className="verify help-needed" href={o.uri}>Help verify observation</a>
+                )}
+              </p>
+            </div>
+            ))}
+          </div>
 
             {this.state.observations.map((o) => (
               <div className="observation" key={o.uri}>
@@ -560,9 +577,11 @@ class App extends Component {
                     <a className="verify help-needed" href={o.uri}>Help verify observation</a>
                   )}
                 </p>
-
               </div>
             ))}
+
+
+
         </div>
       </section>}
 
